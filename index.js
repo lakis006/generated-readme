1c6c9cebc6eb279ef93129e87c673bbe1e6ffbbc
Jsquad <lakis006@umn.edu>
2020-05-30 21:01:37 -0500

    added the readMeQuestion object

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const axios = require('axios');
 const inquirer = require('inquirer');
 
-const questions = [
+const gitHubQuestions = [
     {
         type: "input",
         message: "Enter your Github username",
@@ -11,12 +11,34 @@ const questions = [
             if (input == "") {
                 return "Please enter a valid username"
             } else {
-                return true;
+                true;
             }
         }
     },
     {
-        type: input
+        type: "input",
+        message: "Enter the name of the existing github repository",
+        name: "repository"
+    }
+];
+
+const readMeQuestions = [
+    {
+        type: "input",
+        message: "Enter your Github username",
+        name: "username",
+        validate: function(input) {
+            if (input == "") {
+                return "Please enter a valid username"
+            } else {
+                true;
+            }
+        }
+    },
+    {
+        type: "input",
+        message: "Enter the name of the existing github repository",
+        name: "repository"
     }
 ];
 
