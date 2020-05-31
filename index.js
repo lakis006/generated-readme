34c35e98c87a9f6febbd68d8addbaf3f43422fa5
Jsquad <lakis006@umn.edu>
2020-05-30 20:55:30 -0500

    added a little of the question object

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const questions = [
+const fs = require('fs');
+const axios = require('axios');
+const inquirer = require('inquirer');
 
+const questions = [
+    {
+        type: "input",
+        message: "Enter your Github username",
+        name: "username",
+        validate: function(input) {
+            if (input == "") {
+                return "Please enter a valid username"
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: input
+    }
 ];
 
 function writeToFile(fileName, data) {
